59f4c9ab135c83730a7adb4d5a26d162e1298a8d
HWI <ming037@naver.com>
2017-10-16 18:38:38 +0900

    add random function

--- DPLL.py
+++ DPLL.py
@@ -1,3 +1,5 @@
+import string
+import random
 def simplify(cnf, literal):
     global truth_table
     res = []
@@ -51,33 +53,71 @@ def dpll(cnf):
     # splitting rule
     return splitting_rule(cnf)
 
+
+def random_cnf(k, m, n):
+    global CNF
+    global truth_table
+    global literal_set
+    #  select k liternals from m symbols, generate n clause
+    #  Each clause has independent symbol ex)[[A, !A], [B], [D]] is wrong case
+    #  Each Sentence has independent clause ex)[[A,C], [A,C], [!B]] is wrong case
+    CNF = []
+    posneg = ['', '!']
+    symbols =[]
+    for c in string.ascii_uppercase:
+        symbols.append(c)
+    for c in string.ascii_lowercase:
+        symbols.append(c)
+    symbols = symbols[:n]
+
+    while len(CNF) != m:
+        tmp_clause = []
+        while len(tmp_clause) != k:
+            lit = random.choice(symbols)
+            if lit not in tmp_clause and '!'+lit not in tmp_clause:
+                truth_table[lit] = 0
+                tmp_clause.append(random.choice(posneg) + lit)
+        if tmp_clause not in CNF:
+            CNF.append(tmp_clause)
+    print(CNF)
+
+
 if __name__ == "__main__":
     unit_count = 0
     splitting_count = 0
-    f = open('input.txt', 'r')
     CNF = []
     literal_set = set()
     truth_table = {}
-    clause = f.readline()
-    while clause:
-        tmp = clause.rstrip().split(' ')
-        for c in tmp:
-            if c[0] == '!':
-                literal_set.add(c[1])
-            else:
-                literal_set.add(c)
-        CNF.append(tmp)
+
+    order = input("Choose T(text) or R(random):")
+
+    if order == "T":
+        f = open('input.txt', 'r')
         clause = f.readline()
-    for literal in literal_set:
-        truth_table[literal] = 0
-   
+        while clause:
+            tmp = clause.rstrip().split(' ')
+            for c in tmp:
+                if c[0] == '!':
+                    literal_set.add(c[1])
+                else:
+                    literal_set.add(c)
+            CNF.append(tmp)
+            clause = f.readline()
+        for literal in literal_set:
+            truth_table[literal] = 0
+        f.close()
+    else:
+        random_cnf(3, 10, 50)
+
     dpll_result = dpll(CNF)
     print(dpll_result)
 
     if dpll_result == 'satisfiable':
         truth_table = sorted(truth_table.items())
         for truth in truth_table:
-            print(truth[0], "=", truth[1])
-    print("Unit-propagation Rule count:", unit_count)
-    print("Splitting-count Rule count:", splitting_count)
-    f.close()
+            if truth[1] != 0:
+                print(truth[0], "=", truth[1])
+    print("Unit propagation Rule count:", unit_count)
+    print("Splitting Rule count:", splitting_count)
+
+
